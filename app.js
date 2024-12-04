42a5ad88c9a23293d230f58ad15d51578869f9b7
rasermely1 <143221113+rasermely1@users.noreply.github.com>
2024-12-04 16:55:24 -0600

    change to port 3000

--- app.js
+++ app.js
@@ -16,6 +16,5 @@ app.get('/', (req, res) => {
     res.redirect('/availability');
 });
 
-const PORT = 80;
-const PUBLIC_IP = '3.144.95.103';
-app.listen(PORT, () => console.log(`Server running at http://${PUBLIC_IP}`));
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
